c6b4899efaf7989d5f5a5e7babafd3098e7e014a
Nick McCurdy <nick@nickmccurdy.com>
2019-10-31 12:42:41 -0400

    Extract recommended config generation for testing

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,15 +1,14 @@
-const { rules, configs } = require('../lib/index');
+const { rules, generateRecommendedConfig, configs } = require('../lib/index');
 
 it('should have all the rules', () => {
   expect(rules).toMatchSnapshot();
 });
 
 it('should have a recommended config with recommended rules', () => {
-  for (const [name, rule] of Object.entries(rules)) {
-    if (rule.meta.docs.recommended) {
-      expect(configs.recommended.rules).toHaveProperty(name, 'error');
-    } else {
-      expect(configs.recommended.rules).not.toHaveProperty(name);
-    }
-  }
+  expect(
+    generateRecommendedConfig({
+      good: { meta: { docs: { recommended: true } } },
+      bad: { meta: { docs: { recommended: false } } },
+    })
+  ).toEqual({ good: 'error' });
 });
