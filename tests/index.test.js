ac58a9aa818800c51d352e475d8a6eae06741e0c
Nick McCurdy <nick@nickmccurdy.com>
2019-10-31 12:06:18 -0400

    Add test for recommended config

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,15 @@
-const { rules } = require('../lib/index');
+const { rules, configs } = require('../lib/index');
 
 it('should have all the rules', () => {
   expect(rules).toMatchSnapshot();
+});
+
+it('should have a recommended config with recommended rules', () => {
+  for (const [name, rule] of Object.entries(rules)) {
+    if (rule.meta.docs.recommended) {
+      expect(configs.recommended.rules).toHaveProperty(name, 'error');
+    } else {
+      expect(configs.recommended.rules).not.toHaveProperty(name);
+    }
+  }
 });
