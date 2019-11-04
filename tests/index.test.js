29fbec730a5cc6b021ced1cfca302fcb92a32847
Ben Monro <ben.monro@gmail.com>
2019-11-04 08:34:54 -0800

    revert: "Autogenerate recommended config" (#12)

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,5 @@
-const { rules, generateRecommendedConfig, configs } = require('../lib/index');
+const { rules } = require('../lib/index');
 
 it('should have all the rules', () => {
   expect(rules).toMatchSnapshot();
-});
-
-it('should have a recommended config with recommended rules', () => {
-  expect(
-    generateRecommendedConfig({
-      good: { meta: { docs: { recommended: true } } },
-      bad: { meta: { docs: { recommended: false } } },
-    })
-  ).toEqual({ good: 'error' });
 });
