10b5ac47633488a587859a405efe2f66d6aff5dd
Nick McCurdy <nick@nickmccurdy.com>
2019-11-04 13:12:24 -0500

    Add missing jest-dom prefix to config (fixes #10)

--- tests/index.test.js
+++ tests/index.test.js
@@ -10,5 +10,5 @@ it('should have a recommended config with recommended rules', () => {
       good: { meta: { docs: { recommended: true } } },
       bad: { meta: { docs: { recommended: false } } },
     })
-  ).toEqual({ good: 'error' });
+  ).toEqual({ 'jest-dom/good': 'error' });
 });
