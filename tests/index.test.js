ec23b963419d7a4116f7c0a3d515595db447d998
Nick McCurdy <nick@nickmccurdy.com>
2019-11-06 10:46:30 -0500

    fix: Add missing jest-dom prefix to config (fixes #10) (#13)
    * Revert "Revert "Autogenerate recommended config""
    
    This reverts commit 0512ce6f4ddf4da1aa7a8dd349027e41472bb84a.
    
    * Add missing jest-dom prefix to config (fixes #10)

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,14 @@
-const { rules } = require('../lib/index');
+const { rules, generateRecommendedConfig, configs } = require('../lib/index');
 
 it('should have all the rules', () => {
   expect(rules).toMatchSnapshot();
+});
+
+it('should have a recommended config with recommended rules', () => {
+  expect(
+    generateRecommendedConfig({
+      good: { meta: { docs: { recommended: true } } },
+      bad: { meta: { docs: { recommended: false } } },
+    })
+  ).toEqual({ 'jest-dom/good': 'error' });
 });
