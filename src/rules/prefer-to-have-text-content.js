f5a983d331e4e79767825ba5f08aca58b5d9b4b1
Ben Monro <ben.monro@gmail.com>
2020-07-11 20:43:22 -0400

    fix(to-have-text-content): improved handling non literals and regex in autofix (#65)

--- src/rules/prefer-to-have-text-content.js
+++ src/rules/prefer-to-have-text-content.js
@@ -16,17 +16,31 @@ export const create = (context) => ({
     node
   ) {
     const expectedArg = node.parent.parent.parent.arguments[0];
+
+    const expectedArgSource = context.getSourceCode().getText(expectedArg);
     context.report({
       node: node.parent,
       message: `Use toHaveTextContent instead of asserting on DOM node attributes`,
-      fix: (fixer) => [
-        fixer.removeRange([node.property.range[0] - 1, node.property.range[1]]),
-        fixer.replaceTextRange(
-          node.parent.parent.property.range,
-          "toHaveTextContent"
-        ),
-        fixer.replaceTextRange(expectedArg.range, `/${expectedArg.value}/`),
-      ],
+      fix: (fixer) => {
+        return [
+          fixer.removeRange([
+            node.property.range[0] - 1,
+            node.property.range[1],
+          ]),
+          fixer.replaceTextRange(
+            node.parent.parent.property.range,
+            "toHaveTextContent"
+          ),
+          fixer.replaceTextRange(
+            expectedArg.range,
+            expectedArg.type === "Literal"
+              ? expectedArg.regex
+                ? expectedArgSource
+                : `/${expectedArg.value}/`
+              : `new RegExp(${expectedArgSource})`
+          ),
+        ];
+      },
     });
   },
   [`MemberExpression[property.name='textContent'][parent.callee.name='expect'][parent.parent.property.name=/toBe$|to(Strict)?Equal/]`](
@@ -63,6 +77,7 @@ export const create = (context) => ({
     node
   ) {
     const expectedArg = node.parent.parent.parent.parent.arguments[0];
+    const expectedArgSource = context.getSourceCode().getText(expectedArg);
     context.report({
       node: node.parent,
       message: `Use toHaveTextContent instead of asserting on DOM node attributes`,
@@ -72,7 +87,14 @@ export const create = (context) => ({
           node.parent.parent.parent.property.range,
           "toHaveTextContent"
         ),
-        fixer.replaceTextRange(expectedArg.range, `/${expectedArg.value}/`),
+        fixer.replaceTextRange(
+          expectedArg.range,
+          expectedArg.type === "Literal"
+            ? expectedArg.regex
+              ? expectedArgSource
+              : `/${expectedArg.value}/`
+            : `new RegExp(${expectedArgSource})`
+        ),
       ],
     });
   },
