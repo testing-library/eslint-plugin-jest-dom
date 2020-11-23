4e2bc4eb26f663163ab2efcee022b9ca07859153
Anton Niklasson <anton@finhack.se>
2020-11-23 10:23:33 +0100

    get rid of Array.flat() to support node 10

--- src/__tests__/lib/rules/prefer-in-document.js
+++ src/__tests__/lib/rules/prefer-in-document.js
@@ -27,62 +27,58 @@ function invalidCase(code, output) {
   };
 }
 
+const valid = [
+  ...queries.map((q) => [
+    `expect(screen.${q}('foo')).toBeInTheDocument()`,
+    `expect(${q}('foo')).toBeInTheDocument()`,
+    `expect(wrapper.${q}('foo')).toBeInTheDocument()`,
+  ]),
+  `expect(screen.notAQuery('foo-bar')).toHaveLength(1)`,
+  `expect(screen.getByText('foo-bar')).toHaveLength(2)`,
+];
+const invalid = [
+  // Invalid cases that applies to all variants
+  ...queries.map((q) => [
+    invalidCase(
+      `expect(screen.${q}('foo')).toHaveLength(1)`,
+      `expect(screen.${q}('foo')).toBeInTheDocument()`
+    ),
+    invalidCase(
+      `expect(${q}('foo')).toHaveLength(1)`,
+      `expect(${q}('foo')).toBeInTheDocument()`
+    ),
+    invalidCase(
+      `expect(wrapper.${q}('foo')).toHaveLength(1)`,
+      `expect(wrapper.${q}('foo')).toBeInTheDocument()`
+    ),
+  ]),
+  // Invalid cases that applies to queryBy* and queryAllBy*
+  ...queriesByVariant.query.map((q) => [
+    invalidCase(
+      `expect(${q}('foo')).toHaveLength(0)`,
+      `expect(${q}('foo')).not.toBeInTheDocument()`
+    ),
+    invalidCase(
+      `expect(${q}('foo')).toBeNull()`,
+      `expect(${q}('foo')).not.toBeInTheDocument()`
+    ),
+    invalidCase(
+      `expect(${q}('foo')).not.toBeNull()`,
+      `expect(${q}('foo')).toBeInTheDocument()`
+    ),
+    invalidCase(
+      `expect(${q}('foo')).toBeDefined()`,
+      `expect(${q}('foo')).toBeInTheDocument()`
+    ),
+    invalidCase(
+      `expect(${q}('foo')).not.toBeDefined()`,
+      `expect(${q}('foo')).not.toBeInTheDocument()`
+    ),
+  ]),
+];
+
 const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } });
 ruleTester.run("prefer-in-document", rule, {
-  valid: [
-    ...queries
-      .map((q) => [
-        `expect(screen.${q}('foo')).toBeInTheDocument()`,
-        `expect(${q}('foo')).toBeInTheDocument()`,
-        `expect(wrapper.${q}('foo')).toBeInTheDocument()`,
-      ])
-      .flat(),
-    `expect(screen.notAQuery('foo-bar')).toHaveLength(1)`,
-    `expect(screen.getByText('foo-bar')).toHaveLength(2)`,
-  ],
-  invalid: [
-    // Invalid cases that applies to all variants
-    ...queries
-      .map((q) => [
-        invalidCase(
-          `expect(screen.${q}('foo')).toHaveLength(1)`,
-          `expect(screen.${q}('foo')).toBeInTheDocument()`
-        ),
-        invalidCase(
-          `expect(${q}('foo')).toHaveLength(1)`,
-          `expect(${q}('foo')).toBeInTheDocument()`
-        ),
-        invalidCase(
-          `expect(wrapper.${q}('foo')).toHaveLength(1)`,
-          `expect(wrapper.${q}('foo')).toBeInTheDocument()`
-        ),
-      ])
-      .flat(),
-
-    // Invalid cases that applies to queryBy* and queryAllBy*
-    ...queriesByVariant.query
-      .map((q) => [
-        invalidCase(
-          `expect(${q}('foo')).toHaveLength(0)`,
-          `expect(${q}('foo')).not.toBeInTheDocument()`
-        ),
-        invalidCase(
-          `expect(${q}('foo')).toBeNull()`,
-          `expect(${q}('foo')).not.toBeInTheDocument()`
-        ),
-        invalidCase(
-          `expect(${q}('foo')).not.toBeNull()`,
-          `expect(${q}('foo')).toBeInTheDocument()`
-        ),
-        invalidCase(
-          `expect(${q}('foo')).toBeDefined()`,
-          `expect(${q}('foo')).toBeInTheDocument()`
-        ),
-        invalidCase(
-          `expect(${q}('foo')).not.toBeDefined()`,
-          `expect(${q}('foo')).not.toBeInTheDocument()`
-        ),
-      ])
-      .flat(),
-  ],
+  valid: [].concat(...valid),
+  invalid: [].concat(...invalid),
 });
