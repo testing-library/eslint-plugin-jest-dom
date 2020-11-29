9c845086b1350ecbca2182ba9f0feea180736521
Ben Monro <ben.monro@gmail.com>
2020-11-29 10:49:29 -0800

    move check into create

--- src/rules/prefer-in-document.js
+++ src/rules/prefer-in-document.js
@@ -27,57 +27,60 @@ function isAntonymMatcher(matcherNode, matcherArguments) {
   );
 }
 
-function check(
-  context,
-  { queryNode, matcherNode, matcherArguments, negatedMatcher, expect }
-) {
-  if (!queryNode || (!queryNode.name && !queryNode.property)) return;
-  // toHaveLength() is only invalid with 0 or 1
-  if (matcherNode.name === "toHaveLength" && matcherArguments[0].value > 1) {
-    return;
-  }
-
-  const query = queryNode.name || queryNode.property.name;
-
-  if (queries.includes(query)) {
-    context.report({
-      node: matcherNode,
-      messageId: "use-document",
-      loc: matcherNode.loc,
-      fix(fixer) {
-        const operations = [];
-
-        // Remove any arguments in the matcher
-        for (const argument of Array.from(matcherArguments)) {
-          operations.push(fixer.remove(argument));
-        }
-        // Flip the .not if necessary
-        if (isAntonymMatcher(matcherNode, matcherArguments)) {
-          if (negatedMatcher) {
-            operations.push(
-              fixer.replaceTextRange(
-                [expect.range[1], matcherNode.range[1]],
-                ".toBeInTheDocument"
-              )
-            );
-
-            return operations;
-          } else {
-            operations.push(fixer.insertTextBefore(matcherNode, "not."));
-          }
-        }
-
-        // Replace the actual matcher
-        operations.push(fixer.replaceText(matcherNode, "toBeInTheDocument"));
-
-        return operations;
-      },
-    });
-  }
-}
-
 export const create = (context) => {
   const alternativeMatchers = /(toHaveLength|toBeDefined|toBeNull)/;
+  function check({
+    queryNode,
+    matcherNode,
+    matcherArguments,
+    negatedMatcher,
+    expect,
+  }) {
+    if (!queryNode || (!queryNode.name && !queryNode.property)) return;
+    // toHaveLength() is only invalid with 0 or 1
+    if (matcherNode.name === "toHaveLength" && matcherArguments[0].value > 1) {
+      return;
+    }
+
+    const query = queryNode.name || queryNode.property.name;
+
+    if (queries.includes(query)) {
+      context.report({
+        node: matcherNode,
+        messageId: "use-document",
+        loc: matcherNode.loc,
+        fix(fixer) {
+          const operations = [];
+
+          // Remove any arguments in the matcher
+          for (const argument of Array.from(matcherArguments)) {
+            operations.push(fixer.remove(argument));
+          }
+          // Flip the .not if necessary
+          if (isAntonymMatcher(matcherNode, matcherArguments)) {
+            if (negatedMatcher) {
+              operations.push(
+                fixer.replaceTextRange(
+                  [expect.range[1], matcherNode.range[1]],
+                  ".toBeInTheDocument"
+                )
+              );
+
+              return operations;
+            } else {
+              operations.push(fixer.insertTextBefore(matcherNode, "not."));
+            }
+          }
+
+          // Replace the actual matcher
+          operations.push(fixer.replaceText(matcherNode, "toBeInTheDocument"));
+
+          return operations;
+        },
+      });
+    }
+  }
+
   function getQueryNodeFromAssignment(identifierName) {
     const variable = context.getScope().set.get(identifierName);
     const init = variable.defs[0].node.init;
@@ -119,7 +122,7 @@ export const create = (context) => {
       const matcherArguments = node.arguments;
 
       const expect = node.callee.object.object;
-      check(context, {
+      check({
         negatedMatcher: true,
         queryNode,
         matcherNode,
@@ -141,7 +144,7 @@ export const create = (context) => {
 
       const expect = node.object.object;
 
-      check(context, {
+      check({
         negatedMatcher: true,
         queryNode,
         matcherNode,
@@ -160,7 +163,7 @@ export const create = (context) => {
 
       const matcherArguments = node.parent.arguments;
 
-      check(context, {
+      check({
         negatedMatcher: false,
         queryNode,
         matcherNode,
@@ -178,7 +181,7 @@ export const create = (context) => {
       const matcherNode = node.callee.property;
       const matcherArguments = node.arguments;
 
-      check(context, {
+      check({
         negatedMatcher: false,
         queryNode,
         matcherNode,
