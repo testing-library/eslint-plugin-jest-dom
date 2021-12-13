5bf643636f42b8f324c05d347093033360ccb432
Michaël De Boey <info@michaeldeboey.be>
2021-12-13 23:36:05 +0100

    test: remove `jest-extended` usage (#236)

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,5 +1,4 @@
-import { rules, generateRecommendedConfig } from "../index";
-import "jest-extended";
+import { generateRecommendedConfig, rules } from "../";
 
 it("should have all the rules", () => {
   expect(Object.keys(rules).length).toBeGreaterThan(0);
@@ -8,9 +7,9 @@ it("should have all the rules", () => {
 it.each(Object.keys(rules))("%s should export required fields", (ruleName) => {
   const rule = rules[ruleName];
   expect(rule).toHaveProperty("create", expect.any(Function));
-  expect(rule.meta.docs.url).not.toBeEmpty();
+  expect(rule.meta.docs.url).not.toBe("");
   expect(rule.meta.docs.category).toBe("Best Practices");
-  expect(rule.meta.docs.description).not.toBeEmpty();
+  expect(rule.meta.docs.description).not.toBe("");
 });
 
 it("should have a recommended config with recommended rules", () => {
