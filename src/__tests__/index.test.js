cf0d0e6c05bb7b17b8495500f0b3b4df15a34226
Gareth Jones <jones258@gmail.com>
2024-02-17 08:05:27 +1300

    feat: restructure plugin exports to enable support for flat configs
    BREAKING CHANGE

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,22 +1,76 @@
-import { generateRecommendedConfig, rules } from "../";
+import plugin from "../";
 
 it("should have all the rules", () => {
-  expect(Object.keys(rules).length).toBeGreaterThan(0);
+  expect(Object.keys(plugin.rules).length).toBeGreaterThan(0);
 });
 
-it.each(Object.keys(rules))("%s should export required fields", (ruleName) => {
-  const rule = rules[ruleName];
-  expect(rule).toHaveProperty("create", expect.any(Function));
-  expect(rule.meta.docs.url).not.toBe("");
-  expect(rule.meta.docs.category).toBe("Best Practices");
-  expect(rule.meta.docs.description).not.toBe("");
+it.each(Object.entries(plugin.rules))(
+  "%s should export required fields",
+  (name, rule) => {
+    expect(rule).toHaveProperty("create", expect.any(Function));
+    expect(rule.meta.docs.url).not.toBe("");
+    expect(rule.meta.docs.category).toBe("Best Practices");
+    expect(rule.meta.docs.description).not.toBe("");
+  }
+);
+
+it("has the expected recommended config", () => {
+  expect(plugin.configs.recommended).toMatchInlineSnapshot(`
+    Object {
+      plugins: Array [
+        jest-dom,
+      ],
+      rules: Object {
+        jest-dom/prefer-checked: error,
+        jest-dom/prefer-empty: error,
+        jest-dom/prefer-enabled-disabled: error,
+        jest-dom/prefer-focus: error,
+        jest-dom/prefer-in-document: error,
+        jest-dom/prefer-required: error,
+        jest-dom/prefer-to-have-attribute: error,
+        jest-dom/prefer-to-have-class: error,
+        jest-dom/prefer-to-have-style: error,
+        jest-dom/prefer-to-have-text-content: error,
+        jest-dom/prefer-to-have-value: error,
+      },
+    }
+  `);
 });
 
-it("should have a recommended config with recommended rules", () => {
-  expect(
-    generateRecommendedConfig({
-      good: { meta: { docs: { recommended: true } } },
-      bad: { meta: { docs: { recommended: false } } },
-    })
-  ).toEqual({ "jest-dom/good": "error" });
+it("has the expected recommended flat config", () => {
+  const expectJestDomPlugin = expect.objectContaining({
+    meta: {
+      name: "eslint-plugin-example",
+      version: expect.any(String),
+    },
+  });
+
+  expect(plugin.configs["flat/recommended"]).toMatchInlineSnapshot(
+    { plugins: { "jest-dom": expectJestDomPlugin } },
+    `
+    Object {
+      plugins: Object {
+        jest-dom: ObjectContaining {
+          meta: Object {
+            name: eslint-plugin-example,
+            version: Any<String>,
+          },
+        },
+      },
+      rules: Object {
+        jest-dom/prefer-checked: error,
+        jest-dom/prefer-empty: error,
+        jest-dom/prefer-enabled-disabled: error,
+        jest-dom/prefer-focus: error,
+        jest-dom/prefer-in-document: error,
+        jest-dom/prefer-required: error,
+        jest-dom/prefer-to-have-attribute: error,
+        jest-dom/prefer-to-have-class: error,
+        jest-dom/prefer-to-have-style: error,
+        jest-dom/prefer-to-have-text-content: error,
+        jest-dom/prefer-to-have-value: error,
+      },
+    }
+  `
+  );
 });
--- src/index.js
+++ src/index.js
@@ -14,33 +14,49 @@ import requireIndex from "requireindex";
 //------------------------------------------------------------------------------
 
 // import all rules in src/rules
-export const rules = requireIndex(`${__dirname}/rules`);
+const rules = requireIndex(`${__dirname}/rules`);
 
-export const generateRecommendedConfig = (allRules) =>
-  Object.entries(allRules).reduce(
-    (memo, [name, rule]) => ({
-      ...memo,
-      ...(rule.meta.docs.recommended ? { [`jest-dom/${name}`]: "error" } : {}),
-    }),
-    {}
-  );
+const recommendedRules = Object.entries(rules).reduce(
+  (memo, [name, rule]) => ({
+    ...memo,
+    ...(rule.meta.docs.recommended ? { [`jest-dom/${name}`]: "error" } : {}),
+  }),
+  {}
+);
 
-export const generateAllRulesConfig = (allRules) =>
-  Object.entries(allRules).reduce(
-    (memo, [name]) => ({
-      ...memo,
-      ...{ [`jest-dom/${name}`]: "error" },
-    }),
-    {}
-  );
+const allRules = Object.entries(rules).reduce(
+  (memo, [name]) => ({
+    ...memo,
+    ...{ [`jest-dom/${name}`]: "error" },
+  }),
+  {}
+);
 
-export const configs = {
-  recommended: {
-    plugins: ["jest-dom"],
-    rules: generateRecommendedConfig(rules),
+const plugin = {
+  meta: {
+    name: "eslint-plugin-example",
+    version: "1.0.0",
   },
-  all: {
-    plugins: ["jest-dom"],
-    rules: generateAllRulesConfig(rules),
+  configs: {
+    recommended: {
+      plugins: ["jest-dom"],
+      rules: recommendedRules,
+    },
+    all: {
+      plugins: ["jest-dom"],
+      rules: allRules,
+    },
   },
+  rules,
 };
+
+plugin.configs["flat/recommended"] = {
+  plugins: { "jest-dom": plugin },
+  rules: recommendedRules,
+};
+plugin.configs["flat/all"] = {
+  plugins: { "jest-dom": plugin },
+  rules: allRules,
+};
+
+export default plugin;
