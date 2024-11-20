fe0d731a19ae7db36cc59a42be3cfa12753798d5
Michaël De Boey <info@michaeldeboey.be>
2024-11-20 17:21:59 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,14 @@
 import type { Linter, Rule } from "eslint";
 
-type SupportedConfigs = 'all' | 'recommended';
+type JestDomPluginConfig = 'all' | 'recommended';
 
 declare const plugin: {
   meta: {
     name: string;
     version: string;
   };
-  configs: Record<SupportedConfigs, Linter.LegacyConfig> &
-    Record<`flat/${SupportedConfigs}`, Linter.FlatConfig>;
+  configs: Record<JestDomPluginConfig, Linter.LegacyConfig> &
+    Record<`flat/${JestDomPluginConfig}`, Linter.FlatConfig>;
   rules: Record<string, Rule.RuleModule>;
 };
 
