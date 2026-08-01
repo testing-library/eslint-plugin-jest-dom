371622e3e3c73a13b068f9d6038de38478eb86b4
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-08-01 12:50:16 +1200

    refactor: manage and run Jest directly

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  watchPlugins: [
+    require.resolve("jest-watch-typeahead/filename"),
+    require.resolve("jest-watch-typeahead/testname"),
+  ],
+  snapshotSerializers: [
+    require.resolve("jest-serializer-path"),
+    require.resolve("jest-snapshot-serializer-raw/always"),
+  ],
+  // todo: this can go once we switch to using babel directly
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "kcd-scripts/dist/config/babel-transform",
+  },
+  testPathIgnorePatterns: ["<rootDir>/src/__tests__/__fixtures__/*"],
+};
+
+module.exports = config;
