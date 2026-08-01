7b1328ceef0ccd01bc54d5d132fc7c690ef603cb
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-08-01 12:28:06 +1200

    build: enable loose mode for now

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,12 @@ const supportedNodeVersion = semver.minVersion(pkg.engines.node).version;
 
 /** @type {import('@babel/core').TransformOptions} */
 const config = {
-  presets: [["@babel/preset-env", { targets: { node: supportedNodeVersion } }]],
+  presets: [
+    [
+      "@babel/preset-env",
+      { targets: { node: supportedNodeVersion }, loose: true },
+    ],
+  ],
   ignore: process.env.NODE_ENV === "test" ? [] : ["**/__tests__/**"],
 };
 
